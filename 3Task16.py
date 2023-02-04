14a3deb69fabfc39bae5130f8e7878f96520eb43
HumanBox <stashksenia@gmail.com>
2023-02-04 14:48:22 +0300

    Task 16 shorter

--- 3Task16.py
+++ 3Task16.py
@@ -9,10 +9,5 @@ n = int(input('Укажите желаемую длинну массива: '))
 if n > 0: mumbo_jumbo_list = [randint(1, 10) for i in range(n)]
 else: n = int(input('Требуется целое положительное число (например, 5): '))
 x = int(input('Какое число будем искать: '))
-count = 0
-for i in range(0, len(mumbo_jumbo_list)):
-    if mumbo_jumbo_list[i] == x: count += 1
-    i += 1
-
 print(mumbo_jumbo_list)
-print(f'Число {x} встречается {count} раз')
+print(f'Число {x} встречается {mumbo_jumbo_list.count(x)} раз')
