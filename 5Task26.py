811cccc9ad59f00ee4bb2fd9f71942dec9b61186
HumanBox <stashksenia@gmail.com>
2023-02-10 14:32:46 +0300

    Task 26 with negative

--- 5Task26.py
+++ 5Task26.py
@@ -3,11 +3,16 @@
 # помощью рекурсии
 
 def expo (n, m):
-     if m == 1:
-         return n
-     return expo(n, m-1) * n
+    if m < 0:
+        m *= -1
+    if m == 1:
+        return n
+    return expo(n, m-1) * n
 
 a = int(input('Введите число: '))
 b = int(input('Введите степень: '))
 a_expo = expo(a, b)
-print(a_expo)
+if b > 0:
+    print(a_expo)
+else:
+    print(1 / a_expo)
